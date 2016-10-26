fdf11a78ddb10ef60989364d55b1162a380d1c7c
Dan <dansonlinepresence@gmail.com>
2016-10-25 22:01:58 -0400

    copy-only editing

--- dansvideoeditor.py
+++ dansvideoeditor.py
@@ -13,28 +13,35 @@ class Node:
 				self.end=end
 				return self
 
-		def render(self, width, height, dry=False):
-				render=self._render(width, height)
+		def render(self, width, height, copy=False, dry=False):
+				render=self._render(width, height, copy)
 				invocation='ffmpeg'
-				#input
-				invocation+=' '+render['input_files']
-				#filtergraph
-				invocation+=' -filter_complex "'
-				invocation+=render['reshapes']
-				invocation+=render['concat_links']+'concat=n='+str(render['index']+1)+':v=1:a=1[v][a]'
-				invocation+='"'
-				#output
-				invocation+=' -map [v] -map [a] output.mov'
-				#print/invoke
+				if copy:
+					with open('dansvideoeditor-temp.txt', 'w') as f: f.write(render['input_files'])
+					invocation+=' -safe 0 -f concat -i dansvideoeditor-temp.txt -codec copy'
+				else:
+					invocation+=' '+render['input_files']
+					invocation+=' -filter_complex "'
+					invocation+=render['reshapes']
+					invocation+=render['concat_links']+'concat=n='+str(render['index']+1)+':v=1:a=1[v][a]'
+					invocation+='"'
+					invocation+=' -map [v] -map [a]'
+				invocation+=' output.mov'
 				if dry: print(invocation)
 				else:
 						import subprocess
 						subprocess.check_call(invocation, shell=True)
 
-		def _render(self, width, height, index=0):
-			input_file='-i '+self.file_path
-			if hasattr(self, 'start'):
-				input_file='-ss '+str(self.start)+' -t '+str(self.end-self.start)+' '+input_file
+		def _render(self, width, height, copy, index=0):
+			if copy:
+				input_file='file '+self.file_path+'\n'
+				if hasattr(self, 'start'):
+					input_file+='inpoint '+str(self.start)+'\n'
+					input_file+='outpoint '+str(self.end)+'\n'
+			else:
+				input_file='-i '+self.file_path
+				if hasattr(self, 'start'):
+					input_file='-ss '+str(self.start)+' -t '+str(self.end-self.start)+' '+input_file
 			reshape ='[{i}:v:0]'
 			reshape+='scale=iw*{m}:ih*{m}'
 			reshape+=',pad={w}:{h}:({w}-iw*{m})/2:({h}-ih*{m})/2'
@@ -49,7 +56,7 @@ class Node:
 			}
 			if hasattr(self, 'children'):
 				for child in self.children:
-					x=child._render(width, height, result['index']+1)
+					x=child._render(width, height, copy, result['index']+1)
 					result={i: result[i]+' '+x[i] for i in ['input_files', 'reshapes', 'concat_links']}
 					result['index']=x['index']
 			return result
